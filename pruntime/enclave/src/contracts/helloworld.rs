3d6065b1c53ba2e292542c87284da347542786a7
gdixon <gdixon@assetinfo.co.uk>
2020-10-22 22:06:15 +0100

    Converts Increment to Whisper
    Works against a string value (vector) instead of a int

--- pruntime/enclave/src/contracts/helloworld.rs
+++ pruntime/enclave/src/contracts/helloworld.rs
@@ -7,16 +7,16 @@ use crate::TransactionStatus;
 /// HelloWorld contract states.
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct HelloWorld {
-    counter: u32,
+    whisper: Vec<u8>,
 }
 
 /// The commands that the contract accepts from the blockchain. Also called transactions.
 /// Commands are supposed to update the states of the contract.
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Command {
-    /// Increments the counter in the contract by some number
-    Increment {
-        value: u32,
+    /// Increments the whisper in the contract by some number
+    SetWhisper {
+        value: Vec<u8>,
     },
 }
 
@@ -31,16 +31,16 @@ pub enum Error {
 /// Queries are not supposed to write to the contract states.
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Request {
-    /// Ask for the value of the counter
-    GetCount,
+    /// Ask for the value of the whisper
+    GetWhisper,
 }
 
 /// Query responses.
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Response {
-    /// Returns the value of the counter
-    GetCount {
-        count: u32,
+    /// Returns the value of the whisper
+    GetWhisper {
+        whisper: Vec<u8>,
     },
     /// Something wrong happened
     Error(Error)
@@ -62,9 +62,9 @@ impl contracts::Contract<Command, Request, Response> for HelloWorld {
     fn handle_command(&mut self, _origin: &chain::AccountId, _txref: &TxRef, cmd: Command) -> TransactionStatus {
         match cmd {
             // Handle the `Increment` command with one parameter
-            Command::Increment { value } => {
-                // Simply increment the counter by some value.
-                self.counter += value;
+            Command::SetWhisper { value } => {
+                // Simply increment the whisper by some value.
+                self.whisper = value;
                 // Returns TransactionStatus::Ok to indicate a successful transaction
                 TransactionStatus::Ok
             },
@@ -75,10 +75,10 @@ impl contracts::Contract<Command, Request, Response> for HelloWorld {
     fn handle_query(&mut self, _origin: Option<&chain::AccountId>, req: Request) -> Response {
         let inner = || -> Result<Response, Error> {
             match req {
-                // Hanlde the `GetCount` request.
-                Request::GetCount => {
-                    // Respond with the counter in the contract states.
-                    Ok(Response::GetCount { count: self.counter })
+                // Hanlde the `GetWhisper` request.
+                Request::GetWhisper => {
+                    // Respond with the whisper in the contract states.
+                    Ok(Response::GetWhisper { whisper: self.whisper })
                 },
             }
         };
