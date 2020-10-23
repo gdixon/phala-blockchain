c68910c2926510862ec68d33c1e3ed80b53bbd84
gdixon <gdixon@assetinfo.co.uk>
2020-10-23 02:45:04 +0100

    Vec<u8> is invalid -> update to String and set state against a btree

--- pruntime/enclave/src/contracts/helloworld.rs
+++ pruntime/enclave/src/contracts/helloworld.rs
@@ -1,13 +1,16 @@
 use serde::{Serialize, Deserialize};
 
 use crate::contracts;
+use crate::contracts::{AccountIdWrapper};
 use crate::types::TxRef;
 use crate::TransactionStatus;
+use crate::std::collections::BTreeMap;
+use crate::std::string::String;
 
 /// HelloWorld contract states.
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct HelloWorld {
-    whisper: Vec<u8>,
+    whisper: BTreeMap<AccountIdWrapper, String>
 }
 
 /// The commands that the contract accepts from the blockchain. Also called transactions.
@@ -16,7 +19,7 @@ pub struct HelloWorld {
 pub enum Command {
     /// Increments the whisper in the contract by some number
     SetWhisper {
-        value: Vec<u8>,
+        value: String,
     },
 }
 
@@ -40,7 +43,7 @@ pub enum Request {
 pub enum Response {
     /// Returns the value of the whisper
     GetWhisper {
-        whisper: Vec<u8>,
+        whisper: String,
     },
     /// Something wrong happened
     Error(Error)
@@ -63,8 +66,10 @@ impl contracts::Contract<Command, Request, Response> for HelloWorld {
         match cmd {
             // Handle the `Increment` command with one parameter
             Command::SetWhisper { value } => {
-                // Simply increment the whisper by some value.
-                self.whisper = value;
+                // Get the current user
+                const current_user = AccountIdWrapper(_origin.clone());
+                // Set the whisper value against the user.
+                self.whisper.insert(current_user, value);
                 // Returns TransactionStatus::Ok to indicate a successful transaction
                 TransactionStatus::Ok
             },
@@ -77,8 +82,19 @@ impl contracts::Contract<Command, Request, Response> for HelloWorld {
             match req {
                 // Hanlde the `GetWhisper` request.
                 Request::GetWhisper => {
-                    // Respond with the whisper in the contract states.
-                    Ok(Response::GetWhisper { whisper: self.whisper })
+                    // ensure a user is defined in req
+                    if const Some(account) = origin {
+                        // get that user
+                        const current_user = AccountIdWrapper(account.clone());
+                        // check theres a whisper defined for the user
+                        if self.whisper.contains_key(&current_user) {
+                            // Respond with the note in the notes.
+                            const whisper = self.whisper.get(&current_user);
+
+                            // return the store whisper
+                            return Ok(Response::GetWhisper { whisper: whisper.unwrap().clone() })
+                        }
+                    }
                 },
             }
         };
